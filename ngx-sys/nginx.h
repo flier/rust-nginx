3cc95fa38a8822c6706d48255dfee3f30857f11c
Flier Lu <flier.lu@gmail.com>
2023-10-09 18:01:43 +0800

    move constant under ifdef

--- ngx-sys/nginx.h
+++ ngx-sys/nginx.h
@@ -3,6 +3,8 @@
 
 #ifdef NGX_MODULE
 #include <ngx_module.h>
+
+const char *NGX_RS_MODULE_SIGNATURE = NGX_MODULE_SIGNATURE;
 #endif
 
 #ifdef NGX_EVENT
@@ -11,30 +13,22 @@
 
 #ifdef NGX_HTTP
 #include <ngx_http.h>
-#endif
 
-#ifdef NGX_MAIL
-#include <ngx_mail.h>
-#endif
-
-#ifdef NGX_STREAM
-#include <ngx_stream.h>
-#endif
-
-#ifdef NGX_HTTP
 const size_t NGX_RS_HTTP_MAIN_CONF_OFFSET = NGX_HTTP_MAIN_CONF_OFFSET;
 const size_t NGX_RS_HTTP_SRV_CONF_OFFSET = NGX_HTTP_SRV_CONF_OFFSET;
 const size_t NGX_RS_HTTP_LOC_CONF_OFFSET = NGX_HTTP_LOC_CONF_OFFSET;
 #endif
 
 #ifdef NGX_MAIL
+#include <ngx_mail.h>
+
 const size_t NGX_RS_MAIL_MAIN_CONF_OFFSET = NGX_MAIL_MAIN_CONF_OFFSET;
 const size_t NGX_RS_MAIL_SRV_CONF_OFFSET = NGX_MAIL_SRV_CONF_OFFSET;
 #endif
 
 #ifdef NGX_STREAM
+#include <ngx_stream.h>
+
 const size_t NGX_RS_STREAM_MAIN_CONF_OFFSET = NGX_STREAM_MAIN_CONF_OFFSET;
 const size_t NGX_RS_STREAM_SRV_CONF_OFFSET = NGX_STREAM_SRV_CONF_OFFSET;
 #endif
-
-const char *NGX_RS_MODULE_SIGNATURE = NGX_MODULE_SIGNATURE;
