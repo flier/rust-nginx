eb12d3ea5f4696391ce0e1e7fe399b1a3225639c
Flier Lu <flier.lu@gmail.com>
2023-10-10 19:55:03 +0800

    add more properties

--- ngx-rt/src/http/req.rs
+++ ngx-rt/src/http/req.rs
@@ -3,6 +3,7 @@ use std::{ffi::CStr, ops::Deref, ptr::NonNull, slice};
 use bitflags::bitflags;
 use foreign_types::{foreign_type, ForeignTypeRef};
 use ngx_rt_derive::native_callback;
+use num_enum::FromPrimitive;
 
 use crate::{
     core::{BufRef, ConnRef, ModuleRef, PoolRef, Str},
@@ -168,6 +169,13 @@ impl RequestRef {
 
         /// Counter of blocks held on the request.
         blocked(): u32;
+
+        uri_changes(): u32;
+
+        request_body_file_log_level(): u32;
+
+        limit_conn_status(): u32;
+        limit_req_status(): u32;
     }
 
     str! {
@@ -200,6 +208,106 @@ impl RequestRef {
         content_handler: HandlerFn;
     }
 
+    flag! {
+        aio;
+
+        /// URI with "/." and on Win32 with "//"
+        complex_uri;
+
+        /// URI with "%"
+        quoted_uri;
+
+        /// URI with "+"
+        plus_in_uri;
+
+        /// URI with empty path
+        empty_path_in_uri;
+
+        invalid_header;
+
+        add_uri_to_alias;
+        valid_location;
+        valid_unparsed_uri;
+        uri_changed;
+
+        request_body_in_single_buf;
+        request_body_in_file_only;
+        request_body_in_persistent_file;
+        request_body_in_clean_file;
+        request_body_file_group_access;
+        request_body_no_buffering;
+
+        subrequest_in_memory;
+        waited;
+
+        proxy;
+        bypass_cache;
+        no_cache;
+
+        limit_rate_set;
+        limit_rate_after_set;
+
+        pipeline;
+        chunked;
+
+        /// the output does not require a body.
+        header_only;
+
+        expect_trailers;
+
+        /// whether client connection keepalive is supported.
+        keepalive;
+
+        lingering_close;
+        discard_body;
+        reading_body;
+
+        /// the current request is internal.
+        internal;
+
+        error_page;
+        filter_finalize;
+        post_action;
+        request_complete;
+        request_output;
+
+        /// the output header has already been sent by the request.
+        header_sent;
+
+        expect_tested;
+        root_tested;
+        done;
+        logged;
+
+        /// the output produced in memory buffers rather than files.
+        main_filter_need_in_memory;
+
+        /// the output produced in memory buffers rather than files.
+        filter_need_in_memory;
+
+        /// the request output be produced in temporary buffers, but not in readonly memory buffers or file buffers.
+        filter_need_temporary;
+
+        preserve_body;
+
+        /// a partial response can be sent to the client, as requested by the HTTP Range header.
+        allow_ranges;
+
+        /// a partial response can be sent while a subrequest is being processed.
+        subrequest_ranges;
+
+        /// only a single continuous range of output data can be sent to the client.
+        single_range;
+
+        disable_not_modified;
+        stat_reading;
+        stat_writing;
+        stat_processing;
+
+        background;
+        health_check;
+    }
+
     /// the client HTTP request method.
     pub fn method(&self) -> Method {
         Method::from_bits_truncate(unsafe { self.as_raw().method as u32 })
@@ -226,6 +334,15 @@ impl RequestRef {
             ((v >> 16) as u32, (v & 0xFFFF) as u32)
         }
     }
+
+    pub fn http_state(&self) -> State {
+        unsafe { State::from(self.as_raw().http_state()) }
+    }
+
+    /// Bitmask showing which modules have buffered the output produced by the request.
+    pub fn buffered(&self) -> Buffered {
+        unsafe { Buffered::from_bits_truncate(self.as_raw().buffered()) }
+    }
 }
 
 #[native_callback]
@@ -233,6 +350,34 @@ pub type HandlerFn = fn(req: &RequestRef) -> Result<(), Error>;
 
 #[native_callback]
 pub type EventHandlerFn = fn(req: &RequestRef);
+
+#[repr(u32)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, FromPrimitive)]
+pub enum State {
+    #[default]
+    InitingRequest = ffi::ngx_http_state_e_NGX_HTTP_INITING_REQUEST_STATE,
+    ReadingRequest = ffi::ngx_http_state_e_NGX_HTTP_READING_REQUEST_STATE,
+    ProcessRequest = ffi::ngx_http_state_e_NGX_HTTP_PROCESS_REQUEST_STATE,
+
+    ConnectUpstream = ffi::ngx_http_state_e_NGX_HTTP_CONNECT_UPSTREAM_STATE,
+    WritingUpstream = ffi::ngx_http_state_e_NGX_HTTP_WRITING_UPSTREAM_STATE,
+    ReadingUpstream = ffi::ngx_http_state_e_NGX_HTTP_READING_UPSTREAM_STATE,
+
+    WritingRequest = ffi::ngx_http_state_e_NGX_HTTP_WRITING_REQUEST_STATE,
+    LingeringClose = ffi::ngx_http_state_e_NGX_HTTP_LINGERING_CLOSE_STATE,
+    Keepalive = ffi::ngx_http_state_e_NGX_HTTP_KEEPALIVE_STATE,
+}
+
+bitflags! {
+    pub struct Buffered: u32 {
+        const LOWLEVEL = ffi::NGX_HTTP_LOWLEVEL_BUFFERED;
+        const WRITE = ffi::NGX_HTTP_WRITE_BUFFERED;
+        const GZIP = ffi::NGX_HTTP_GZIP_BUFFERED;
+        const SSI = ffi::NGX_HTTP_SSI_BUFFERED;
+        const SUB = ffi::NGX_HTTP_SUB_BUFFERED;
+        const COPY = ffi::NGX_HTTP_COPY_BUFFERED;
+    }
+}
 
 foreign_type! {
     pub unsafe type HeadersIn: Send {
